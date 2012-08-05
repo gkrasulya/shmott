09a17f3ab42030864a61e97c713982bd4ed38b4c
Georgy Krasulya <gkrasulya@gmail.com>
2012-08-05 19:25:52 +0400

    many new pages

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 */
 
 
+(function() {
+	$('input[placeholder], textarea[placeholder]');
+
+	$('#faqList .js-turner').click(function() {
+		$(this).parent().toggleClass('opened');
+
+		return false;
+	});
+
+	$('#faqList .js-close').click(function() {
+		$(this).parent().parent().removeClass('opened');
+
+		return false;
+	});
+})();
