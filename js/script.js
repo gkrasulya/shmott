ed71772a1775d1374b30002d0b0adb70985b6258
Georgy Krasulya <gkrasulya@gmail.com>
2012-08-12 19:54:26 +0400

    product pages, fixes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@
 
 
 (function() {
-	$('input[placeholder], textarea[placeholder]');
+	var $b = $('body');
+
+	if ($().placeholder) {
+		$('input[placeholder], textarea[placeholder]').placeholder();
+	}
 
 	$('#faqList .js-turner').click(function() {
 		$(this).parent().toggleClass('opened');
@@ -19,8 +23,61 @@
 	});
 
 	if ($().mCustomScrollbar) {
-		$('.js-custom-scrollbar').mCustomScrollbar({
-
-		})
+		$('.js-custom-scrollbar').mCustomScrollbar();
 	}
+
+	var $allDropdowns = $();
+	$('.js-dropdown').each(function(i, el) {
+		$allDropdowns = $allDropdowns.add($(el));
+		$(el).on('click', '.js-dropdown-turner', function() {
+			if (! $(el).hasClass('opened')) {
+				$allDropdowns.removeClass('opened');
+			}
+			$(el).toggleClass('opened');
+			return false;
+		});
+	});
+
+	var
+	$contactsFormEmailInput = $('#contactsFormEmailInput'),
+	$contactsFormPhoneInput = $('#contactsFormPhoneInput'),
+	$contactsPhoneIcon = $('#contactsPhoneIcon'),
+	$contactsEmailIcon = $('#contactsEmailIcon');
+
+	$contactsFormEmailInput.on('change', function() {
+		if ($(this).val().trim() == '') {
+			$contactsEmailIcon.hide();
+		} else {
+			$contactsEmailIcon.show();
+		}
+	});
+
+	$contactsFormPhoneInput.on('change', function() {
+		if ($(this).val().trim() == '') {
+			$contactsPhoneIcon.hide();
+		} else {
+			$contactsPhoneIcon.show();
+		}
+	});
+
+	$('.js-tabs-wrap').each(function(i, el) {
+		var
+		$links = $(el).find('.js-tabs-links-wrap a'),
+		$tabs = $(el).find('.js-tabs-tab');
+
+		$links.eq(0).addClass('selected');
+		$tabs.eq(0).show();
+
+		$links.each(function(i, link) {
+			$(link).on('click', function() {
+				$links.removeClass('selected');
+				$tabs.hide();
+
+				$(link).addClass('selected');
+				$tabs.eq(i).show();
+
+				return false;
+			});
+		});
+	});
 })();
